4217472aa2f5c8c9852e98ca0072dbe61f44be0d
h3zTr0n <alison.mukoma@yahoo.com>
2016-01-09 23:57:55 +0200

    created the display object for the views

--- spida/apps/display/views.py
+++ spida/apps/display/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Suppliers
+from django.views.generic import DetailView
 
-# Create your views here.
+
+class DisplaySupplierView(DetailView):
+    template_name = "display/display/"
+    obj_name = Suppliers.objects.get()
+
+    def show_content_on_page(self, *args, **kwargs):
+        super(DisplaySupplierView, self).__init__(*args, kwargs)
+        for item in obj_name:
+            if item == 'username':
+                return item
+
+        context = {"logos": obj_name}
+        supplier_title = " Welcome to {0}". format(item)
+        return  render(template_name, context)
