5996478cfe1f5d3b8c310ad02c6f158cc804dae5
h3zTr0n <alison.mukoma@yahoo.com>
2016-01-11 00:30:55 +0200

    initial display rendered

--- spida/urls.py
+++ spida/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^', "spida.apps.display.urls", name="home"),
+	url(r'^', "spida.apps.display.views.HomeDisplayView", name="home"),
     url(r'^admin/', admin.site.urls),
 ]
