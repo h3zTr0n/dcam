ae01a10cdfc40f3552cc78e6f5be2412a7927227
h3zTr0n <alison.mukoma@yahoo.com>
2016-01-10 03:25:12 +0200

    Added user accounts system to views

--- spida/apps/accounts/views.py
+++ spida/apps/accounts/views.py
@@ -1,3 +1,76 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from djang.views import generic
+from django.contrib.auth import get_user_model
+from django.contrib import auth
+from django.contrib import messages
+from authtools import views as authviews
+from braces import views as bracesviews
+from django.conf import settings
+from . import forms
 
-# Create your views here.
+user =get_user_model()
+
+# For user login
+class LoginView(bracesviews.AnonymousRequiredMixin,
+                authviews.LoginView):
+    template_name = "accounts/login.html"
+    form_class = forms.LoginForm
+
+    # checking for validations
+    def form_valid(self, form):
+        redirect = super(LoginView, self).form_valid(form)
+        remember_me = form.cleaned_data.get('remember_me')
+        if remember_me is True:
+            ONE_MONTH = 30*24*60*60
+            expiry = getattr(settings, "KEEP_LOGGED_DURATION", ONE_MONTH)
+            self.request.session.set_expiry(expiry)
+        return redirect
+
+class LogoutView(authviews.LogoutVIew):
+    url = reverse_lazy('index') # redirected page after success logout
+
+# new user signup object
+class signUpView(bracesviews,AnonymousRequierdMixin,
+                 bracesviews.FOrmValidMessageMixin,
+                 generic.CreateView):
+    form_class = fors.SignUpForm
+    model = User
+    template_name = "accounts/signup.html"
+    success_url = reverse_lazy("index")
+    form_valid_message = "You're signed up!"
+
+    def form_valid(self, form):
+        r = super(SignUpView, self).from_valid(form)
+        username = form.cleaned_data["email"]
+        password = form.cleaned_data["password1"]
+        user = auth.authenticate(email=username, password=password)
+        auth.login(self.request, user)
+        return r
+
+class PasswordChangeView(authviews.PasswordChangeView):
+    form_class = forms.PasswordChangeForm
+    template_name = "accounts/password-change.html"
+    success_url = reverse_lazy("index")
+
+    def form_valid(self, from):
+        form.save()
+        messages.success(self.request,
+                         "Your password was changed,"
+                         "hence you have been logged out. Please relogin")
+        return super(PasswordChangeVIew, self).form_valid(form)
+
+class PasswordResetView(authviews.PasswordResetView,):
+    form_class = forms.PasswordResetForm
+    template_name = "accounts/password-reset.html"
+    success_url = reverse_lazy("accounts:password-reset-done")
+    subject_template_name = "accounts/emails/password-reset-done.txt"
+    email_template_name = "accounts/emails/password-reset-email.html"
+
+class PasswordResetDoneView(authviews.PasswordResetDoneView):
+    template_name = "accounts/password-reset-done.html"
+
+
+class PasswordResetCOnfirmView(authviews.PasswordResetAndLoginView):
+    template_name = "accounts/password-reset-confirm,html"
+    form_class = forms.SetPasswordForm
