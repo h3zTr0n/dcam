d6d1a520f62f3648810acf5bed7eae221bb89f1f
h3zTr0n <alison.mukoma@yahoo.com>
2016-01-12 01:50:35 +0200

    collecting statistics for wagtail

--- spida/settings.py
+++ spida/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), "static")
 
 MEDIA_ROOT = (BASE_DIR)
 MEDIA_URL = '/uploads/'
