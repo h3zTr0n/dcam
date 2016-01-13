37f11468917a1c82024101b1480e38a9f51f235b
h3zTr0n <alison.mukoma@yahoo.com>
2016-01-13 19:50:15 +0200

    Initial commit formyBuyList app views files

--- spida/apps/myBuyList/views.py
+++ spida/apps/myBuyList/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+class ShopList(generic.TemplateView):
+    template_name = "myBuyList/home.html"
+
+# Method for user shopping list description
+class SignUpView(generic.CreateView):
+    # form_class = UserCreationForm
+    # model = User
+    template_name = "myBuyList/signup.html"
--- spida/urls.py
+++ spida/urls.py
@@ -16,25 +16,28 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
-from django.conf.urls import include
-from django.contrib import admin
-from spida.apps.display import urls as display_urls
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Integrating wagtail content manager
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailcore import urls as wagtail_urls
-
+#######################################
+from django.conf.urls import url
+from django.conf.urls import include
+from django.contrib import admin
+from spida.apps.display import urls as display_urls
+from spida.apps.myBuyList import urls as shop_urls
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
-	url(r'^home/', include(display_urls), name="home"),
-#	url(r'^/home/', "spida.apps.display.views.HomeDisplayView.as_view()", name="home"),
-    url(r'^admin/', admin.site.urls),
+	url(r'^home/', include(display_urls), name="home"),    url(r'^admin/', admin.site.urls),
     url(r'^cms/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
     url(r'^pages/', include(wagtail_urls)),
+    url(r'accounts/register/', include(shop_urls), name="signup"),
+    url(r'^shoplist/', include(shop_urls), name="shoplist"),
+    #url(r'^register/', 'spida.apps.myBuyList.views.SignUpView.as_view()', name="Sign Up"),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
