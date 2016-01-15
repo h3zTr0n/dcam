12f17aed597c55e5b0dc3d36ecf3b024cc147848
h3zTr0n <alison.mukoma@yahoo.com>
2016-01-15 20:43:06 +0200

    Base code

--- spida/apps/shopList/forms.py
+++ spida/apps/shopList/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, ButtonHolder, Submit
 
@@ -12,7 +12,20 @@ class RegistrationForm(UserCreationForm):
             'Password1',
             'Password2',
             ButtonHolder(
-                Submit('register', 'Register', css_class="btn-primary")
+            Submit('register', 'Register', css_class="btn-primary")
 
             )
         )
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper
+        self.helper.layout = Layout(
+            'username',
+            'password',
+            ButtonHolder(
+            Submit('login', 'Login', css_class="btn-primary")
+            )
+        )
+print '42minutes kenneth love'
--- spida/apps/shopList/views.py
+++ spida/apps/shopList/views.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import
 
+from django.contrib.auth import authenticate, login, logout
+from django.core.urlresolvers import reverse_lazy
 from django.views import generic
 from django.contrib.auth.models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 
 # Create your views here.
 
@@ -13,3 +15,19 @@ class SignUpView(generic.CreateView):
     form_class = RegistrationForm
     user = User
     template_name = "accounts/signup.html"
+
+class LoginView(generic.FormView):
+    form_class = LoginForm
+    success_url = reverse_lazy('home')
+    template_name = "accounts/login.html"
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data.get['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None and user.is_active:
+            login(self.request, user)
+            return super(LoginView, self).form_valid(form)
+        else:
+            return self.form_invalid(form)
